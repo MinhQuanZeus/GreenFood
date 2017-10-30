c2bd73b7afdf5415c2313ea080af32d308ea8b95
QuanTM9 <quantm9@fsoft.com.vn>
2017-10-30 23:42:44 +0700

    init project

---
+++ app/src/main/java/greenlife/com/vn/greenfood/activities/FoodDetailActivity.java
@@ -0,0 +1,15 @@
+package greenlife.com.vn.greenfood.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import greenlife.com.vn.greenfood.R;
+
+public class FoodDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_detail);
+    }
+}
